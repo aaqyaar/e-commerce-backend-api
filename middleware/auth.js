e31b2915e1af4aabce1454d3f4db4ab6ca3276e4
abdizamedmo <abdizamedmo@gmail.com>
2022-10-06 16:44:08 +0300

    auth middlewares setted to if user isAuthorized to manage the app

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,11 @@
 const { JWT } = require("../config/JWT");
+const Role = require("../models/roles.model");
+const User = require("../models/user.model");
+
+const getPermissions = (role) => {
+  const r = Role.findOne({ name: role }).exec();
+  return r.permissions;
+};
 
 exports.isAuth = async (req, res, next) => {
   try {
@@ -16,4 +23,30 @@ exports.isAuth = async (req, res, next) => {
   } catch (error) {
     res.status(500).send(error);
   }
+};
+
+exports.isAuthorized = (...permissions) => {
+  return async (req, res, next) => {
+    try {
+      const { email } = req.user;
+      const user = await User.findOne({ email }).exec();
+
+      if (!user) {
+        return res.status(404).send({ error: "User not found" });
+      }
+      if (!user.role)
+        return res.status(404).send({ error: "User role not found" });
+
+      let userPermissions = await getPermissions(user.role);
+
+      if (
+        user &&
+        permissions.every((permission) => userPermissions.includes(permission))
+      )
+        return next();
+      res.status(403).json({ message: "Forbidden" });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
 };
